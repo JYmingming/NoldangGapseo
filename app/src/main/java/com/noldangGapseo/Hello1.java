476184fe9ad1495e10a3d362be19585b2728a919
bingsoo95 <kimminsu403@naver.com>
2022-03-10 18:05:38 +0900

    bingsoo

--- app/src/main/java/com/noldangGapseo/Hello1.java
+++ app/src/main/java/com/noldangGapseo/Hello1.java
@@ -2,7 +2,7 @@ package com.noldangGapseo;
 
 public class Hello1 {
   public static void main(String[] args) {
-    System.out.println("전영민");
+    System.out.println("전영민1");
     System.out.println("망치살인");
   }
 }
