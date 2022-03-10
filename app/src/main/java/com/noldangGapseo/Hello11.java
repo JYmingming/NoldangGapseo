611338fb15d935a6c6a21a30905b6731224a8947
JYmingming <sla5962@gmail.com>
2022-03-10 17:46:18 +0900

    Merge branch 'main' of https://github.com/JYmingming/NoldangGapseo # Please enter a commit message to explain why this merge is necessary, # especially if it merges an updated upstream into a topic branch. # # Lines starting with '#' will be ignored, and an empty message aborts # the commit.

--- app/src/main/java/com/noldangGapseo/Hello11.java
+++ app/src/main/java/com/noldangGapseo/Hello11.java
@@ -1,7 +1,7 @@
 package com.noldangGapseo;
 
-public class Hello1 {
+public class Hello11 {
   public static void main(String[] args) {
-    System.out.println("전영민");
+    System.out.println("전영");
   }
 }
