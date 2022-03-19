de16797142c56f6d18cb958f01aa6da8434a40be
ggabdoll <ggabdoll0327@naver.com>
2022-03-20 03:24:52 +0900

    qequestMapping

--- app/src/main/java/com/noldangGapseo/controller/UserController.java
+++ app/src/main/java/com/noldangGapseo/controller/UserController.java
@@ -3,23 +3,25 @@ package com.noldangGapseo.controller;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.noldangGapseo.dao.UserDao;
 import com.noldangGapseo.domain.User;
 
 @RestController
+@RequestMapping("/user")
 public class UserController {
 
   @Autowired
   UserDao userDao;
 
   //유저의 전체 리스트를 가져온다ㅎㅎ
-  @GetMapping("/user/list")
+  @GetMapping("/list")
   public List<User> list(){
     return userDao.findAll();
   }
 
-  @GetMapping("/user/search")
+  @GetMapping("/search")
   public User search(String nickName) {
     return userDao.findNickname(nickName);
   }
