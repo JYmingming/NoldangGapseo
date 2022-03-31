30f7f3033266d622acd67726aabcc039237bf2e2
ggabdoll <ggabdoll0327@naver.com>
2022-03-31 21:25:09 +0900

    todo structure

--- app/src/main/resources/static/travel/travelInfo/travelInfo.js
+++ app/src/main/resources/static/travel/travelInfo/travelInfo.js
@@ -1,8 +1,28 @@
+//여행이름 모달
 $('.travel-name').click(function (e) {
     e.preventDefault();
     $('#nameModal').modal('show');
 });
+
+//
+
+//모달 닫기
 $('.btn-close').click(function (e) {
     e.preventDefault();
     e.stopPropagation();
+});
+
+$('.invite-box').click(function (e) {
+    e.preventDefault();
+    $('#compModal').modal('show');
+});
+
+$('.travel-weather').click(function (e) {
+    e.preventDefault();
+    $('#weatherModal').modal('show');
+});
+
+$('.travel-cost').click(function (e) {
+    e.preventDefault();
+    window.open('./cost/travelCost.html');
 });
