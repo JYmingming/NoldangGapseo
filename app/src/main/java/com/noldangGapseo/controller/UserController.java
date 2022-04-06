24c4a7d314ae70d6313dfc5ca63d77ef3e99f43e
ggabdoll <ggabdoll0327@naver.com>
2022-04-07 01:55:21 +0900

    /travel/myboard/form.html update

--- app/src/main/java/com/noldangGapseo/controller/UserController.java
+++ app/src/main/java/com/noldangGapseo/controller/UserController.java
@@ -6,9 +6,7 @@ import com.noldangGapseo.domain.ApiResponse;
 import com.noldangGapseo.domain.UserResponse;
 import com.noldangGapseo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import com.noldangGapseo.dao.UserDao;
 import com.noldangGapseo.domain.User;
 
@@ -16,10 +14,11 @@ import com.noldangGapseo.domain.User;
 @RequestMapping("/user")
 public class UserController {
 
+
   @Autowired
   UserService service;
 
-  @RequestMapping("/signup")
+  @GetMapping("/signup")
   public ApiResponse signUp(User user){
     ApiResponse apires =new ApiResponse();
     if(service.add(user)==1){
