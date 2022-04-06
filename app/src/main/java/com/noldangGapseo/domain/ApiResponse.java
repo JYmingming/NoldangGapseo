352811c093bdc0920d5ecefd8bacde3db2ea5824
ggabdoll <ggabdoll0327@naver.com>
2022-04-06 17:40:54 +0900

    update Apiresponse

--- app/src/main/java/com/noldangGapseo/domain/ApiResponse.java
+++ app/src/main/java/com/noldangGapseo/domain/ApiResponse.java
@@ -9,6 +9,10 @@ import lombok.*;
 @AllArgsConstructor
 @EqualsAndHashCode(callSuper=false)
 public class ApiResponse {
+// 실패시
+// resCode = "1111"
+// resStatus = "fail"
+
 
  private String resCode = "0000";
  private String resStatus = "success";
