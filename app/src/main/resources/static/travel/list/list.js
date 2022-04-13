d54aef0fd7386756d71d19ff475012db8f75deea
ggabdoll <ggabdoll0327@naver.com>
2022-04-13 17:43:39 +0900

    travelList api

--- app/src/main/resources/static/travel/list/list.js
+++ app/src/main/resources/static/travel/list/list.js
@@ -1,7 +1,19 @@
+import { travelList} from '../../common/api/apiList.js';
+
+// --- stiky color ---
+const stikyColor = ['blue', 'green', 'yello', 'brown', 'purple', 'orange'];
+
+//console.log('dddd');
+
 $('.content-card').on('click', function (e) {
     e.preventDefault();
     location.href = '/travel/travelInfo/travelInfo.html';
 });
+
+(async function () {
+    const response = await travelList('정창성장판이안닫혀서성장통이심해');
+    console.log('reponse:::', response);
+})();
 
 $('.choice1 .btn1').on('click', function (e) {
     e.preventDefault();
