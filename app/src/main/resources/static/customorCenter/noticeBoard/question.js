91b0023b6523a5feda82f99b1c076c5f60fe4a9e
kihyuns <qkdro78@naver.com>
2022-04-14 16:26:29 +0900

    페이지 수정, 추가

---
+++ app/src/main/resources/static/customorCenter/noticeBoard/question.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){  
+  document.getElementById("registration").onclick = function() {
+        document.getElementById("modal").style.display="block";
+    }
+   
+    document.getElementById("modal_close_btn").onclick = function() {
+        document.getElementById("modal")
+    }   
+ })
