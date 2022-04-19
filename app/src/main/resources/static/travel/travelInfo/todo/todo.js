57c04084e55b7147403457d1ed0f4c275d33d968
ggabdoll <ggabdoll0327@naver.com>
2022-04-19 19:59:52 +0900

    todo complete

--- app/src/main/resources/static/travel/travelInfo/todo/todo.js
+++ app/src/main/resources/static/travel/travelInfo/todo/todo.js
@@ -1,4 +1,10 @@
-import { todoList } from '../../../common/api/apiList.js';
+import {
+    todoList,
+    addTodo,
+    setTodoStatus,
+    updateTodo,
+    deleteTodo,
+} from '../../../common/api/apiList.js';
 
 // ---- URLSearchParams ----
 var arr = location.href.split('?');
@@ -23,23 +29,67 @@ if (no == null) {
     // ---- 화면 렌더링 ----
     const list = await todoList(no);
     list?.map((m) => {
-        console.log(m);
         const todoView = `<div class="content-box w-50 d-flex flex-column align-items-center">
             <div class="todo-col w-100 d-flex justify-content-sm-around align-items-center"
             data-id=${m.todoId}
             >
-                <input type="checkbox" class="form-check-input" value=${m.status}/>
-                <div class="c-cost">${m.name}</div>
+                <input type="checkbox" class="form-check-input"
+                    ${m.status == 0 ? '' : 'checked'}/>
+                <input type="text" class="t-todo w-75" value="${m.name}" />
+                <div class="delete-btn">❌</div>
             </div>
         </div>`;
         $('.todo-content').append(todoView);
     });
 })();
+// ===== todo 추가 =====
+const newTodo = $('.input-name');
 
-$('.input-name').on('keyup', function (key) {
-    if (key.keyCode == 13) {
-        if ($('.input-name').val() == '') {
-            alert('jdkldjkl;;');
+// ---- todo 추가 함수 ----
+const addNewTodo = async (id, newName) => {
+    if (newName == '') {
+        Swal.fire({
+            icon: 'error',
+            title: 'Todo 항목을 작성해 주세요',
+            text: 'something is missing',
+        });
+    } else {
+        const todoRes = await addTodo(id, newName);
+        if (todoRes?.resCode == '0000') {
+            location.reload();
         }
     }
+};
+
+// ---- todo 추가 이벤트 -----
+newTodo.on('keyup', function (key) {
+    if (key.keyCode == 13) {
+        addNewTodo(no, newTodo.val());
+    }
+});
+
+$('.confirm-btn').on('click', function (e) {
+    addNewTodo(no, newTodo.val());
+});
+
+// ===== todo상태 변경 ====
+
+$(document).on('click', '.form-check-input', async function (e) {
+    const todoId = $(this).closest('div').attr('data-id');
+    const status = $(this).is(':checked') ? 1 : 0;
+    setTodoStatus(todoId, status);
+});
+
+// ==== todo 항목 업데이트 ====
+$(document).on('change', '.t-todo', function (e) {
+    const todoId = $(this).closest('div').attr('data-id');
+    const text = $(this).val();
+    updateTodo(todoId, text);
+});
+
+// ==== todo 지우기 ====
+$(document).on('click', '.delete-btn', async function (e) {
+    const todoId = $(this).closest('.todo-col').attr('data-id');
+    const response = await deleteTodo(todoId);
+    $(this).closest('.content-box').remove();
 });
