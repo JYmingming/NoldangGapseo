84d089e4facfa0fc4d0dd0569d7d533033e5568a
KDW1231124 <area153@naver.com>
2022-04-20 19:46:36 +0900

    0420

--- app/src/main/java/com/noldangGapseo/controller/DestinationController.java
+++ app/src/main/java/com/noldangGapseo/controller/DestinationController.java
@@ -1,12 +1,16 @@
 package com.noldangGapseo.controller;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import com.noldangGapseo.domain.Destination;
 import com.noldangGapseo.domain.DestinationResponse;
 import com.noldangGapseo.service.DestinationService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RequestMapping("/destination")
 @RestController
