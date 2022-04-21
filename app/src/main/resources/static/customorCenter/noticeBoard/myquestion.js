804f9f1f5e52545a2cf3e0a3f5fd05230e7b2357
kihyuns <qkdro78@naver.com>
2022-04-21 14:14:34 +0900

    내 질문관리

---
+++ app/src/main/resources/static/customorCenter/noticeBoard/myquestion.js
@@ -0,0 +1,18 @@
+//검색어
+$(document).ready(function(){
+    $.ajax({ 
+      url: "/notice/list",
+      type:"post",
+      data:{type:'Z'},
+       success : function(data) {
+        $.each(data, function(i, dat) {
+          if (dat.view_count == "0") {
+              answer = "접수";
+            }else {
+              answer = "답변완료"
+            }
+          $("#tb1").append("<tr><td>"+(i+1)+"</td><td>"+dat.title+"</td><td>"+dat.nick_name+"</td><td>"+dat.reg_date.substring(0, 10)+"</td><td>"+answer+"</td></tr>");
+        });
+      },
+    });
+});
