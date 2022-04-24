f50785a9a5b6fbab1e8eb389511c8a21e7df3827
ggabdoll <ggabdoll0327@naver.com>
2022-04-24 23:33:52 +0900

    make travel

--- app/src/main/resources/static/indexPage/index.js
+++ app/src/main/resources/static/indexPage/index.js
@@ -1,27 +1,28 @@
-
 // 로그인 세션 확인
-css(".login", "display", "none");
-css(".admin-login", "display", "none");
+css('.login', 'display', 'none');
+css('.admin-login', 'display', 'none');
 
-fetch("/user/getLoginUser").then(function(response) {
-    return response.json();
-}).then(function(result) {
-    if (result.resStatus == "success") {
-        document.querySelector("#user-name").innerHTML = result.data.nickName;
-        css(".login", "display", "");
-        css(".not-login", "display", "none");
-        console.log("로그인");
-    }
-    if (result.data.userId==1){
-        console.log("어드민 로그인");
-        css(".admin-login", "display", "");
+fetch('/user/getLoginUser')
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (result) {
+        if (result.resStatus == 'success') {
+            document.querySelector('#user-name').innerHTML = result.data.nickName;
+            css('.login', 'display', '');
+            css('.not-login', 'display', 'none');
+            console.log('로그인');
+            travel.uesrId = result.data.userId;
+        }
+        if (result.data.userId == 1) {
+            console.log('어드민 로그인');
+            css('.admin-login', 'display', '');
+        }
+    });
 
-    }
-});
-
-document.querySelector("#logout-btn").onclick = function() {
-    fetch("/user/signout").then(function(response) {
-        location.href = "index.html";
+document.querySelector('#logout-btn').onclick = function () {
+    fetch('/user/signout').then(function (response) {
+        location.href = 'index.html';
     });
 };
 
@@ -33,83 +34,90 @@ function css(selector, name, value) {
     }
 }
 
-
 // 로그인
-var xEmail = document.querySelector("input[name=email]");
-var xPassword = document.querySelector("input[name=password]");
+var xEmail = document.querySelector('input[name=email]');
+var xPassword = document.querySelector('input[name=password]');
 
-document.querySelector("form[name=form1]").onsubmit = function() {
-    if ( xEmail.value == "" ||
-        xPassword.value == "") {
-        window.alert("필수 입력 항목이 비어 있습니다.");
+document.querySelector('form[name=form1]').onsubmit = function () {
+    if (xEmail.value == '' || xPassword.value == '') {
+        window.alert('필수 입력 항목이 비어 있습니다.');
         return false;
     }
-    var fd = new FormData(document.forms.namedItem("form1"));
+    var fd = new FormData(document.forms.namedItem('form1'));
 
-    fetch("/user/signin", {
-        method: "POST",
-        body: new URLSearchParams(fd)
+    fetch('/user/signin', {
+        method: 'POST',
+        body: new URLSearchParams(fd),
     })
-        .then(function(response) {
+        .then(function (response) {
             return response.json();
         })
-        .then(function(result) {
-            if (result.resStatus == "success") {
-                location.href = "/indexPage/index.html";
+        .then(function (result) {
+            if (result.resStatus == 'success') {
+                location.href = '/indexPage/index.html';
             } else {
-                window.alert("로그인 실패!")
+                window.alert('로그인 실패!');
             }
         });
     return false;
 };
-document.querySelector("#go_home").onclick = function() {
-    location.href = "/indexPage/index.html";
-}
-
-
+document.querySelector('#go_home').onclick = function () {
+    location.href = '/indexPage/index.html';
+};
 
 // 카카오맵 api 설정
 var container = document.getElementById('map');
 var options = {
     center: new kakao.maps.LatLng(33.374, 126.557),
-    level: 9
+    level: 9,
 };
 var map = new kakao.maps.Map(container, options);
 
-
 //datepicker
 
-$(function() {
+$(function () {
     //모든 datepicker에 대한 공통 옵션 설정
     $.datepicker.setDefaults({
-        dateFormat: 'yy-mm-dd' //Input Display Format 변경
-        ,showOtherMonths: true //빈 공간에 현재월의 앞뒤월의 날짜를 표시
-        ,showMonthAfterYear:true //년도 먼저 나오고, 뒤에 월 표시
-        ,changeYear: false //콤보박스에서 년 선택 가능
-        ,changeMonth: false //콤보박스에서 월 선택 가능
+        dateFormat: 'yy-mm-dd', //Input Display Format 변경
+        showOtherMonths: true, //빈 공간에 현재월의 앞뒤월의 날짜를 표시
+        showMonthAfterYear: true, //년도 먼저 나오고, 뒤에 월 표시
+        changeYear: false, //콤보박스에서 년 선택 가능
+        changeMonth: false, //콤보박스에서 월 선택 가능
         //,showOn: "both" //button:버튼을 표시하고,버튼을 눌러야만 달력 표시 ^ both:버튼을 표시하고,버튼을 누르거나 input을 클릭하면 달력 표시
-        ,buttonImage: "http://jqueryui.com/resources/demos/datepicker/images/calendar.gif" //버튼 이미지 경로
-        ,buttonImageOnly: true //기본 버튼의 회색 부분을 없애고, 이미지만 보이게 함
-        ,buttonText: "선택" //버튼에 마우스 갖다 댔을 때 표시되는 텍스트
+        buttonImage: 'http://jqueryui.com/resources/demos/datepicker/images/calendar.gif', //버튼 이미지 경로
+        buttonImageOnly: true, //기본 버튼의 회색 부분을 없애고, 이미지만 보이게 함
+        buttonText: '선택', //버튼에 마우스 갖다 댔을 때 표시되는 텍스트
         /*,yearSuffix: "년" //달력의 년도 부분 뒤에 붙는 텍스트*/
-        ,monthNamesShort: ['1','2','3','4','5','6','7','8','9','10','11','12'] //달력의 월 부분 텍스트
-        ,monthNames: ['1월','2월','3월','4월','5월','6월','7월','8월','9월','10월','11월','12월'] //달력의 월 부분 Tooltip 텍스트
-        ,dayNamesMin: ['일','월','화','수','목','금','토'] //달력의 요일 부분 텍스트
-        ,dayNames: ['일요일','월요일','화요일','수요일','목요일','금요일','토요일'] //달력의 요일 부분 Tooltip 텍스트
-        ,minDate: "-0D" //최소 선택일자(-1D:하루전, -1M:한달전, -1Y:일년전)
-        ,maxDate: "+3M" //최대 선택일자(+1D:하루후, -1M:한달후, -1Y:일년후)
+        monthNamesShort: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'], //달력의 월 부분 텍스트
+        monthNames: [
+            '1월',
+            '2월',
+            '3월',
+            '4월',
+            '5월',
+            '6월',
+            '7월',
+            '8월',
+            '9월',
+            '10월',
+            '11월',
+            '12월',
+        ], //달력의 월 부분 Tooltip 텍스트
+        dayNamesMin: ['일', '월', '화', '수', '목', '금', '토'], //달력의 요일 부분 텍스트
+        dayNames: ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'], //달력의 요일 부분 Tooltip 텍스트
+        minDate: '-0D', //최소 선택일자(-1D:하루전, -1M:한달전, -1Y:일년전)
+        maxDate: '+3M', //최대 선택일자(+1D:하루후, -1M:한달후, -1Y:일년후)
     });
 
     //input을 datepicker로 선언
-    $("#datepicker_start").datepicker();
-    $("#datepicker_end").datepicker();
+    $('#datepicker_start').datepicker();
+    $('#datepicker_end').datepicker();
 
     //From의 초기값을 오늘 날짜로 설정
     $('#datepicker_start').datepicker('setDate', 'today'); //(-1D:하루전, -1M:한달전, -1Y:일년전), (+1D:하루후, -1M:한달후, -1Y:일년후)
     //To의 초기값을 내일로 설정
     $('#datepicker_end').datepicker('setDate', '+1D'); //(-1D:하루전, -1M:한달전, -1Y:일년전), (+1D:하루후, -1M:한달후, -1Y:일년후)
 });
-
 
 // 4여행지
 import { get4Des } from '../common/api/apiList.js';
@@ -128,3 +136,17 @@ import { get4Des } from '../common/api/apiList.js';
         $('.col-3').children('div').eq(i).append(imageView);
     }
 })();
+
+const travel = {
+    startDate: '',
+    endDate: '',
+    travelName: '',
+    uesrId: '',
+};
+
+// 여행 이름을 설정 하고 확인 버튼을 누르면
+// travel.startDate = startDate.value;
+// travel.endDate = enddate.value;
+// travel.travelName = trvelName.value;
+
+// => travel을 넣어주면 됌
