bb300dbc4785ad33840e6595e72bc10a8cd7023c
kihyuns <qkdro78@naver.com>
2022-04-26 23:48:54 +0900

    수정

--- app/src/main/java/com/noldangGapseo/controller/NoticeBoardController.java
+++ app/src/main/java/com/noldangGapseo/controller/NoticeBoardController.java
@@ -4,11 +4,9 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 import com.noldangGapseo.domain.NoticeVO;
 import com.noldangGapseo.service.NoticeBoardService;
 
@@ -26,15 +24,8 @@ public class NoticeBoardController {
     String type = request.getParameter("type");
     List<NoticeVO> list = service.getNoticeList(type);
 
+
     return list;
   }
 
-  @GetMapping("/announcement")
-  public ModelAndView announcement(){
-    ModelAndView mav = new ModelAndView();
-    List<NoticeVO> list = service.getNoticeList("A");
-    mav.addObject("list", list);
-    mav.setViewName("customorCenter/noticeBoard/announcement.jsp");
-    return mav;
-  }
 }
